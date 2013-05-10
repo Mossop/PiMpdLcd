a94d196e49af5f4993513f936778fcbd0a80ff8b
Dave Townsend <dtownsend@oxymoronical.com>
2013-05-10 13:22:27 -0700

    Scroll long lines.

--- pimpdlcd.py
+++ pimpdlcd.py
@@ -1,5 +1,6 @@
 from select import select
 from mpd import MPDClient
+from threading import Timer
 
 try:
     from lcddevice import OutputDevice
@@ -10,33 +11,103 @@ HOST = 'officepi'
 PORT = '6600'
 PASSWORD = False
 
+WIDTH = 16
+SCROLL_TIME = 1
+
 def center(str):
-    while len(str) < 15:
+    while len(str) < (WIDTH - 1):
         str = " " + str + " "
-    if len(str) < 16:
+    if len(str) < WIDTH:
         str = str + " "
     return str
 
+def build_line(str, pos):
+    if len(str) == WIDTH:
+        return str
+
+    if len(str) < WIDTH:
+        line = "".ljust(pos) + str
+        return line.ljust(WIDTH)
+
+    return str[pos:pos + WIDTH]
+
+def next_dir(str, pos, dir):
+    if len(str) == WIDTH:
+        return 0
+
+    if dir == 0:
+        return 1
+
+    if len(str) < WIDTH:
+        if dir > 0 and (len(str) + pos) == WIDTH:
+            dir = -1
+        elif dir < 0 and pos == 0:
+            dir = 1
+    else:
+        if dir > 0 and len(str) - pos == WIDTH:
+            dir = -1
+        elif dir < 0 and pos == 0:
+            dir = 1
+
+    return dir
+
 class Display(OutputDevice):
     def __init__(self):
-        OutputDevice.__init__(self)
-
         self._status = "stop"
         self._song = {}
+        self._timer = None
+        self._line1 = ""
+        self._line2 = ""
+
+        OutputDevice.__init__(self)
+
+    def on(self):
+        OutputDevice.on(self)
+        if not self._timer:
+            self._timer = Timer(SCROLL_TIME, self._update_scroll)
+            self._timer.start()
 
     def off(self):
         self._status = "stop"
+        if self._timer:
+            self._timer.cancel()
+            self._timer = None
         OutputDevice.off(self)
 
+    def _update_scroll(self):
+        self._timer = None
+
+        self._line1pos = self._line1pos + self._line1dir
+        self._line2pos = self._line2pos + self._line2dir
+        self._line1dir = next_dir(self._line1, self._line1pos, self._line1dir)
+        self._line2dir = next_dir(self._line2, self._line2pos, self._line2dir)
+
+        self._timer = Timer(SCROLL_TIME, self._update_scroll)
+        self._timer.start()
+
+        self.display(build_line(self._line1, self._line1pos),
+                     build_line(self._line2, self._line2pos))
+
     def _update(self):
-        self._line1 = center(self._song["title"])
+        line1 = self._song["title"]
         if self._status == "pause":
-            self._line2 = center("PAUSED")
+            line2 = "PAUSED"
         else:
-            self._line2 = center(self._song["artist"])
-        self._line1pos = 0
-        self._line2pos = 0
-        self.display(self._line1, self._line2)
+            line2 = self._song["artist"]
+
+        if line1 != self._line1:
+            self._line1 = line1
+            self._line1pos = 0
+            self._line1dir = 0
+
+        if line2 != self._line2:
+            self._line2 = line2
+            self._line2pos = 0
+            self._line2dir = 0
+
+        if self._timer:
+            self._timer.cancel()
+        self._update_scroll()
 
     def set_status(self, value):
         if value == "stop":
