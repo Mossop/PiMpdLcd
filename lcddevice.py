3ccf2cfaa7b84fd4babfd6c7477f8ed4a6d49764
Dave Townsend <dtownsend@oxymoronical.com>
2013-05-09 19:16:09 -0700

    Clear LCD first and center text.

--- lcddevice.py
+++ lcddevice.py
@@ -1,5 +1,10 @@
 from adafruit.Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
+def center(str):
+    while len(str) < 15:
+        str = " " + str + " "
+    return str
+
 class Output(object):
     def __init__(self):
         self.lcd = Adafruit_CharLCDPlate()
@@ -7,4 +12,5 @@ class Output(object):
         self.lcd.backlight(self.lcd.RED)
 
     def display(self, line1 = "", line2 = ""):
-        self.lcd.message(line1[:16] + "\n" + line2[:16])
+        self.lcd.clear()
+        self.lcd.message(center(line1[:16]) + "\n" + center(line2[:16]))
